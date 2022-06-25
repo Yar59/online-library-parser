0eddf9575e9620f41fb0cbd214a3ffe03e26dfae
Yar59 <75260156+Yar59@users.noreply.github.com>
2022-06-25 21:10:15 +0300

    books divided into pages

--- render_website.py
+++ render_website.py
@@ -1,9 +1,11 @@
 import json
+import os
 
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 from more_itertools import chunked
 
 JSON_PATH = './books_params.json'
+PAGES_DIR = './pages'
 
 
 def main():
@@ -13,14 +15,19 @@ def main():
         loader=FileSystemLoader('.'),
         autoescape=select_autoescape(['html'])
     )
-    template = env.get_template('template.html')
-    paired_books = chunked(books_params, 2)
-    rendered_page = template.render(
-        paired_books=paired_books
-    )
+    template = env.get_template('./templates/template.html')
 
-    with open('index.html', 'w', encoding="utf8") as file:
-        file.write(rendered_page)
+    books_pages = chunked(books_params, 10)
+
+    os.makedirs(PAGES_DIR, exist_ok=True)
+
+    for number, books_page in enumerate(books_pages):
+        rendered_page = template.render(
+            paired_books=chunked(books_page, 2)
+        )
+        page_path = f'{PAGES_DIR}/index{number}.html'
+        with open(page_path, 'w', encoding="utf8") as file:
+            file.write(rendered_page)
 
 
 if __name__ == '__main__':
