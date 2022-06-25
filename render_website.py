5e5ae5f9e1f4931b31c91fc138816891bf6b1b92
Yar59 <75260156+Yar59@users.noreply.github.com>
2022-06-25 16:14:16 +0300

    added rendering page with jinja

--- render_website.py
+++ render_website.py
@@ -2,14 +2,26 @@ import json
 import os
 import logging
 
-import jinja2
+from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 JSON_PATH = './books_params.json'
 
 
 def main():
-    with open(JSON_PATH, 'r') as file:
+    with open(JSON_PATH, 'r', encoding='UTF-8') as file:
         books_params = json.loads(file.read())
+    env = Environment(
+        loader=FileSystemLoader('.'),
+        autoescape=select_autoescape(['html'])
+    )
+    template = env.get_template('template.html')
+
+    rendered_page = template.render(
+        books=books_params
+    )
+
+    with open('index.html', 'w', encoding="utf8") as file:
+        file.write(rendered_page)
 
 
 if __name__ == '__main__':
