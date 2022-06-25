dbd471504824623a03c55bef1a3de352a1bfa679
Yar59 <75260156+Yar59@users.noreply.github.com>
2022-06-25 22:02:12 +0300

    added pagination

--- render_website.py
+++ render_website.py
@@ -17,15 +17,17 @@ def main():
     )
     template = env.get_template('./templates/template.html')
 
-    books_pages = chunked(books_params, 10)
-
+    books_pages = list(chunked(books_params, 10))
+    total_pages = len(books_pages)
     os.makedirs(PAGES_DIR, exist_ok=True)
 
     for number, books_page in enumerate(books_pages):
         rendered_page = template.render(
-            paired_books=chunked(books_page, 2)
+            paired_books=chunked(books_page, 2),
+            page_number=number+1,
+            total_pages=total_pages,
         )
-        page_path = f'{PAGES_DIR}/index{number}.html'
+        page_path = f'{PAGES_DIR}/index{number + 1}.html'
         with open(page_path, 'w', encoding="utf8") as file:
             file.write(rendered_page)
 
